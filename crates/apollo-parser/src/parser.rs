d685915855a7162a79aad63703312c2ac466dccf
Irina Shestak <shestak.irina@gmail.com>
2021-04-24 19:49:56 +0200

    add AST printing

--- crates/apollo-parser/src/parser.rs
+++ crates/apollo-parser/src/parser.rs
@@ -3,7 +3,8 @@ use rowan::{GreenNode, GreenNodeBuilder};
 #[derive(Debug, Clone, Copy, PartialEq)]
 #[repr(u16)]
 pub enum SyntaxKind {
-    Root = 0,
+    // Whitespace
+    Whitespace = 0,
 
     // Symbols
     Bang,     // !
@@ -28,6 +29,9 @@ pub enum SyntaxKind {
     Node,
     Int,
     Float,
+
+    // Root node
+    Root,
 }
 
 impl From<SyntaxKind> for rowan::SyntaxKind {
@@ -125,30 +129,39 @@ impl Parser {
     fn parse_directive(&mut self) -> Result<(), ()> {
         self.builder.start_node(SyntaxKind::Directive.into());
         self.bump();
+        self.parse_whitespace();
+
         match self.peek() {
-            Some(kind @ SyntaxKind::At) => self.insert_node(kind),
+            Some(SyntaxKind::At) => self.bump(),
             _ => return Err(()),
         }
         match self.peek() {
-            Some(kind @ SyntaxKind::Node) => self.insert_node(kind),
+            Some(SyntaxKind::Node) => self.bump(),
             _ => return Err(()),
         }
+
+        self.parse_whitespace();
         match self.peek() {
-            Some(kind @ SyntaxKind::On) => self.insert_node(kind),
+            Some(SyntaxKind::On) => self.bump(),
             _ => return Err(()),
         }
+
+        self.parse_whitespace();
         match self.peek() {
-            Some(kind @ SyntaxKind::Node) => self.insert_node(kind),
+            Some(SyntaxKind::Node) => self.bump(),
             _ => return Err(()),
         }
         self.builder.finish_node();
         Ok(())
     }
 
-    pub fn insert_node(&mut self, kind: SyntaxKind) {
-        self.builder.start_node(kind.into());
-        self.bump();
-        self.builder.finish_node();
+    pub fn parse_whitespace(&mut self) {
+        let mut text = String::new();
+        while let Some(SyntaxKind::Whitespace) = self.peek() {
+            let (_, s) = self.tokens.pop().unwrap();
+            text.push_str(&s);
+        }
+        self.builder.token(SyntaxKind::Whitespace.into(), &text);
     }
 
     pub fn bump(&mut self) {
@@ -170,11 +183,40 @@ mod test {
         // directive @example on FIELD
         let parser = Parser::new(vec![
             (SyntaxKind::Directive, "directive".to_string()),
+            (SyntaxKind::Whitespace, " ".to_string()),
             (SyntaxKind::At, "@".to_string()),
             (SyntaxKind::Node, "example".to_string()),
+            (SyntaxKind::Whitespace, " ".to_string()),
             (SyntaxKind::On, "on".to_string()),
+            (SyntaxKind::Whitespace, " ".to_string()),
             (SyntaxKind::Node, "FIELD".to_string()),
         ]);
-        dbg!(parser.parse());
+
+        let ParseResult { green_node, .. } = parser.parse();
+        let ast = SyntaxNode::new_root(green_node);
+        print(0, ast.into());
+    }
+
+    type SyntaxNode = rowan::SyntaxNode<super::Language>;
+    #[allow(unused)]
+    type SyntaxToken = rowan::SyntaxToken<super::Language>;
+    #[allow(unused)]
+    type SyntaxElement = rowan::NodeOrToken<SyntaxNode, SyntaxToken>;
+
+    fn print(indent: usize, element: SyntaxElement) {
+        let kind: SyntaxKind = element.kind().into();
+        print!("{:indent$}", "", indent = indent);
+        match element {
+            rowan::NodeOrToken::Node(node) => {
+                println!("- {:?}@{:?}", kind, node.text_range());
+                for child in node.children_with_tokens() {
+                    print(indent + 2, child);
+                }
+            }
+
+            rowan::NodeOrToken::Token(token) => {
+                println!("- {:?}@{:?} {:?}", kind, token.text_range(), token.text())
+            }
+        }
     }
 }
