f75ee6c270280a01157266d5a1f42ba7e5f628e7
Irina Shestak <shestak.irina@gmail.com>
2021-07-05 17:09:32 +0200

    Stub out AST generated files

--- crates/apollo-parser/src/ast/mod.rs
+++ crates/apollo-parser/src/ast/mod.rs
@@ -2,6 +2,8 @@ use std::marker::PhantomData;
 
 use crate::{SyntaxKind, SyntaxNode, SyntaxNodeChildren, SyntaxToken};
 
+mod generated;
+
 /// The main trait to go from untyped `SyntaxNode`  to a typed ast. The
 /// conversion itself has zero runtime cost: ast and syntax nodes have exactly
 /// the same representation: a pointer to the tree root and a pointer to the
