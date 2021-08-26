2d13559374986cb4ec4106a20bbbfbf435ee7cd8
Irina Shestak <shestak.irina@gmail.com>
2021-08-26 12:11:33 +0200

    add fielddefinition parsin

--- crates/apollo-parser/src/parser/field.rs
+++ crates/apollo-parser/src/parser/field.rs
@@ -1,4 +1,4 @@
-use crate::parser::{argument, directive, name, selection};
+use crate::parser::{argument, directive, name, selection, ty};
 use crate::{create_err, Parser, SyntaxKind, TokenKind};
 
 /// See: https://spec.graphql.org/June2018/#Field
@@ -54,4 +54,72 @@ pub(crate) fn field(parser: &mut Parser) {
 pub(crate) fn fields_definition(parser: &mut Parser) {
     let _guard = parser.start_node(SyntaxKind::FIELDS_DEFINITION);
     parser.bump(SyntaxKind::L_CURLY);
+    field_definition(parser);
+    if let Some(TokenKind::RCurly) = parser.peek() {
+        parser.bump(SyntaxKind::L_CURLY)
+    } else {
+        parser.push_err(create_err!(
+            parser
+                .peek_data()
+                .unwrap_or_else(|| String::from("no further data")),
+            "Expected Fields Definition to have a closing }}, got {}",
+            parser
+                .peek_data()
+                .unwrap_or_else(|| String::from("no further data"))
+        ));
+    }
+}
+
+/// See: https://spec.graphql.org/June2018/#FieldDefinition
+///
+/// ```txt
+/// FieldDefinition
+///     Description[opt] Name ArgumentsDefinition[opt] : Type Directives[Const][opt]
+/// ```
+pub(crate) fn field_definition(parser: &mut Parser) {
+    if let Some(TokenKind::Node) = parser.peek() {
+        let guard = parser.start_node(SyntaxKind::FIELD_DEFINITION);
+        name::name(parser);
+        if let Some(TokenKind::LParen) = parser.peek() {
+            argument::arguments_definition(parser);
+        }
+
+        if let Some(TokenKind::Colon) = parser.peek() {
+            parser.bump(SyntaxKind::COLON);
+            match parser.peek() {
+                Some(TokenKind::Node) | Some(TokenKind::LBracket) => {
+                    ty::ty(parser);
+                    if let Some(TokenKind::At) = parser.peek() {
+                        directive::directives(parser);
+                    }
+                    if parser.peek().is_some() {
+                        guard.finish_node();
+                        return field_definition(parser);
+                    }
+                }
+                _ => {
+                    parser.push_err(create_err!(
+                        parser.peek_data().unwrap(),
+                        "Expected InputValue definition to have a Type, got {}",
+                        parser.peek_data().unwrap()
+                    ));
+                }
+            }
+        } else {
+            parser.push_err(create_err!(
+                parser.peek_data().unwrap(),
+                "Expected Field Definition to have a Type, got {}",
+                parser.peek_data().unwrap()
+            ));
+        }
+    }
+
+    if let Some(TokenKind::Comma) = parser.peek() {
+        parser.bump(SyntaxKind::COMMA);
+        return field_definition(parser);
+    }
+
+    if let Some(TokenKind::RCurly) = parser.peek() {
+        return;
+    }
 }
--- crates/apollo-parser/src/parser/ty_definition.rs
+++ crates/apollo-parser/src/parser/ty_definition.rs
@@ -161,4 +161,48 @@ mod test {
             "#,
         )
     }
+
+    #[test]
+    fn it_parses_object_type_definition() {
+        utils::check_ast(
+            "
+            type Person implements Human {
+              name: String
+              age: Int
+              picture: Url
+            }",
+            r#"
+            - DOCUMENT@0..44
+                - OBJECT_TYPE_DEFINITION@0..44
+                    - type_KW@0..4 "type"
+                    - NAME@4..10
+                        - IDENT@4..10 "Person"
+                    - IMPLEMENTS_INTERFACES@10..25
+                        - implements_KW@10..20 "implements"
+                        - NAME@20..25
+                            - IDENT@20..25 "Human"
+                    - FIELDS_DEFINITION@25..44
+                        - L_CURLY@25..26 "{"
+                        - FIELD_DEFINITION@26..31
+                            - NAME@26..30
+                                - IDENT@26..30 "name"
+                            - COLON@30..31 ":"
+                            - TYPE@31..31
+                                - NAMED_TYPE@31..31
+                        - FIELD_DEFINITION@31..35
+                            - NAME@31..34
+                                - IDENT@31..34 "age"
+                            - COLON@34..35 ":"
+                            - TYPE@35..35
+                                - NAMED_TYPE@35..35
+                        - FIELD_DEFINITION@35..43
+                            - NAME@35..42
+                                - IDENT@35..42 "picture"
+                            - COLON@42..43 ":"
+                            - TYPE@43..43
+                                - NAMED_TYPE@43..43
+                        - L_CURLY@43..44 "}"
+            "#,
+        )
+    }
 }
