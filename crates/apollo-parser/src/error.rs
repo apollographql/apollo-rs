e3fcd7b29b109416dc8c8632f819ce8dadcf8fb2
Irina Shestak <shestak.irina@gmail.com>
2021-11-23 13:29:21 -0700

    add doc level comments for parser::Error

--- crates/apollo-parser/src/error.rs
+++ crates/apollo-parser/src/error.rs
@@ -1,6 +1,36 @@
 use std::fmt;
 
 /// An `Error` type for operations performed in the lexer and the parser.
+///
+/// Errors get returned alongside the resulting AST if either the lexer or the
+/// parser encouter lexical or syntactical errors respectively.
+///
+/// We encourage you to check for the AST's errors before proceeding to iterate
+/// over the AST's nodes:
+///
+/// ```rust
+/// use apollo_parser::Parser;
+///
+/// fn main() {
+///     let input = "union SearchResult = Photo | Person | Cat | Dog";
+///     let parser = Parser::new(input);
+///     let ast = parser.parse();
+///
+///     assert!(ast.errors().is_empty());
+///
+///     let doc = ast.document();
+/// }
+///
+/// ```
+///
+/// `Error` struct does not at the moment implement `Display`. We encourage you
+/// to take a look at the [miette] and [annotate_snippets] integration examples
+/// if you'd like to display Errors in a terminal-like environment. The
+/// `Error`'s API allows for you to configure other ways to render your errors
+/// as well if a terminal-like environment is not where your final output goes.
+///
+/// [miette]: https://github.com/apollographql/apollo-rs/blob/main/crates/apollo-parser/examples/miette.rs
+/// [annotate_snippets]: https://github.com/apollographql/apollo-rs/blob/main/crates/apollo-parser/examples/annotate_snippet.rs
 #[derive(PartialEq, Eq, Clone)]
 pub struct Error {
     pub(crate) message: String,
@@ -27,12 +57,14 @@ impl Error {
         }
     }
 
-    /// Get a reference to the error's data.
+    /// Get a reference to the error's data. This is usually the token that
+    /// `apollo-parser` has found to be lexically or syntactically incorrect.
     pub fn data(&self) -> &str {
         self.data.as_ref()
     }
 
-    /// Get a reference to the error's index.
+    /// Get a reference to the error's index. This is where the error begins in
+    /// a given input.
     pub fn index(&self) -> usize {
         self.index
     }
