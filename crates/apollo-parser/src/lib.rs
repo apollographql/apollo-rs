d313cdb62cd9965ad8b70a24ed1452d5c9bd1808
Irina Shestak <shestak.irina@gmail.com>
2022-01-26 17:08:58 +0100

    apollo-parser@0.2.1 (#154)

--- crates/apollo-parser/src/lib.rs
+++ crates/apollo-parser/src/lib.rs
@@ -31,7 +31,7 @@
 //! ```toml
 //! # Just an example, change to the necessary package version.
 //! [dependencies]
-//! apollo-parser = "0.2.0"
+//! apollo-parser = "0.2.1"
 //! ```
 //!
 //! Or using [cargo-edit]:
