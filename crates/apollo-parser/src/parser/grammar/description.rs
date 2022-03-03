92ed9c1908a7e92bc4e68211dd88372697c15090
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2022-03-03 17:24:46 +0100

    apollo-parser: add description support for SchemaDefinition
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- crates/apollo-parser/src/parser/grammar/description.rs
+++ crates/apollo-parser/src/parser/grammar/description.rs
@@ -9,3 +9,42 @@ pub(crate) fn description(p: &mut Parser) {
     let _g_string = p.start_node(SyntaxKind::STRING_VALUE);
     p.bump(SyntaxKind::STRING)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::ast;
+
+    use super::*;
+    #[test]
+    fn it_can_access_arguments_in_fields() {
+        let schema = r#"
+"""
+What a dumb description
+"""
+type Query {
+  bestSellers(category: ProductCategory = ALL): [Product] @join__field(graph: PRODUCTS)
+}
+        "#;
+        let parser = Parser::new(schema);
+        let ast = parser.parse();
+
+        assert!(ast.errors.is_empty());
+
+        let document = ast.document();
+        for definition in document.definitions() {
+            if let ast::Definition::ObjectTypeDefinition(obj_def) = definition {
+                assert_eq!(
+                    obj_def
+                        .description()
+                        .unwrap()
+                        .string_value()
+                        .unwrap()
+                        .to_string(),
+                    "\"\"\"\nWhat a dumb description\n\"\"\"\n"
+                );
+                return;
+            }
+        }
+        panic!("object type definition has not been catched");
+    }
+}
