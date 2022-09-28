d26d2623cc72048a30ee2c3013f1f206a9125a83
Renée Kooi <renee@kooi.me>
2022-09-28 16:26:35 +0200

    check directive arguments for dupes

--- crates/apollo-compiler/src/validation/arguments.rs
+++ crates/apollo-compiler/src/validation/arguments.rs
@@ -5,37 +5,48 @@ use crate::{
     ApolloDiagnostic, ValidationDatabase,
 };
 
+fn check_input_value_definition_uniqueness(db: &dyn ValidationDatabase, input_values: &[InputValueDefinition], diagnostics: &mut Vec<ApolloDiagnostic>) {
+    let mut seen: HashMap<&str, &InputValueDefinition> = HashMap::new();
+    for input_value in input_values {
+        let name = input_value.name();
+        if let Some(prev_def) = seen.get(name) {
+            let prev_offset: usize = prev_def.ast_node(db.upcast()).unwrap().text_range().start().into();
+            let prev_node_len: usize = prev_def.ast_node(db.upcast()).unwrap().text_range().len().into();
+
+            let current_offset: usize = input_value.ast_node(db.upcast()).unwrap().text_range().start().into();
+            let current_node_len: usize = input_value.ast_node(db.upcast()).unwrap().text_range().len().into();
+
+            diagnostics.push(ApolloDiagnostic::UniqueDefinition(UniqueDefinition {
+                ty: "argument".into(),
+                name: name.into(),
+                src: db.input(),
+                original_definition: (prev_offset, prev_node_len).into(),
+                redefined_definition: (current_offset, current_node_len).into(),
+                help: Some(format!("`{name}` argument must only be defined once.")),
+            }));
+        } else {
+            seen.insert(name, &input_value);
+        }
+    }
+}
+
 pub fn check(db: &dyn ValidationDatabase) -> Vec<ApolloDiagnostic> {
     let mut diagnostics = Vec::new();
 
-    for object_type in db.object_types().iter() {
-        for field in object_type.fields_definition() {
-            let mut seen: HashMap<&str, &InputValueDefinition> = HashMap::new();
-            let input_values = field.arguments().input_values();
-            for input_value in input_values {
-                let name = input_value.name();
-                if let Some(prev_def) = seen.get(name) {
-                    let prev_offset: usize = prev_def.ast_node(db.upcast()).unwrap().text_range().start().into();
-                    let prev_node_len: usize = prev_def.ast_node(db.upcast()).unwrap().text_range().len().into();
+    let object_types = db.object_types();
+    let directive_definitions = db.directive_definitions();
 
-                    let current_offset: usize = input_value.ast_node(db.upcast()).unwrap().text_range().start().into();
-                    let current_node_len: usize = input_value.ast_node(db.upcast()).unwrap().text_range().len().into();
+    let object_input_values = object_types.iter()
+        .flat_map(|object_type| object_type.fields_definition())
+        .map(|field| field.arguments().input_values());
 
-                    diagnostics.push(ApolloDiagnostic::UniqueDefinition(UniqueDefinition {
-                        ty: "argument".into(),
-                        name: name.into(),
-                        src: db.input(),
-                        original_definition: (prev_offset, prev_node_len).into(),
-                        redefined_definition: (current_offset, current_node_len).into(),
-                        help: Some(format!(
-                            "`{name}` argument must only be defined once."
-                        )),
-                    }));
-                } else {
-                    seen.insert(name, &input_value);
-                }
-            }
-        }
+    let directive_input_values = directive_definitions.iter()
+        // Builtin directives do not have a backing AST Node. We don't need to check those.
+        .filter(|directive_definition| directive_definition.ast_node(db.upcast()).is_some())
+        .map(|directive_definition| directive_definition.arguments().input_values());
+
+    for input_values in object_input_values.chain(directive_input_values) {
+        check_input_value_definition_uniqueness(db, input_values, &mut diagnostics);
     }
 
     diagnostics
@@ -46,13 +57,24 @@ mod test {
     use crate::ApolloCompiler;
 
     #[test]
-    fn it_fails_validation_with_duplicate_argument_names() {
+    fn it_fails_validation_with_duplicate_field_argument_names() {
         let input = r#"
 type Query {
   single(arg: Boolean): Int
   duplicate(arg: Boolean, arg: Boolean): Int
 }
 "#;
+        let ctx = ApolloCompiler::new(input);
+        let diagnostics = ctx.validate();
+        for diagnostic in &diagnostics {
+            println!("{}", diagnostic)
+        }
+        assert_eq!(diagnostics.len(), 1);
+    }
+
+    #[test]
+    fn it_fails_validation_with_duplicate_directive_argument_names() {
+        let input = r#"directive @example(arg: Boolean, arg: Boolean) on FIELD"#;
         let ctx = ApolloCompiler::new(input);
         let diagnostics = ctx.validate();
         for diagnostic in &diagnostics {
