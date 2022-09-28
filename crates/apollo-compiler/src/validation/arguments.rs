2cb1fb4d02880626597cebce0f27ded3eee8f183
Renée Kooi <renee@kooi.me>
2022-09-28 14:47:47 +0200

    implement unique argument check for field definitions

--- crates/apollo-compiler/src/validation/arguments.rs
+++ crates/apollo-compiler/src/validation/arguments.rs
@@ -1,7 +1,44 @@
-use crate::{ApolloDiagnostic, ValidationDatabase};
+use std::collections::HashMap;
+use crate::{
+    diagnostics::UniqueDefinition,
+    hir::InputValueDefinition,
+    ApolloDiagnostic, ValidationDatabase,
+};
 
-pub fn check(_db: &dyn ValidationDatabase) -> Vec<ApolloDiagnostic> {
-    todo!()
+pub fn check(db: &dyn ValidationDatabase) -> Vec<ApolloDiagnostic> {
+    let mut diagnostics = Vec::new();
+
+    for object_type in db.object_types().iter() {
+        for field in object_type.fields_definition() {
+            let mut seen: HashMap<&str, &InputValueDefinition> = HashMap::new();
+            let input_values = field.arguments().input_values();
+            for input_value in input_values {
+                let name = input_value.name();
+                if let Some(prev_def) = seen.get(name) {
+                    let prev_offset: usize = prev_def.ast_node(db.upcast()).unwrap().text_range().start().into();
+                    let prev_node_len: usize = prev_def.ast_node(db.upcast()).unwrap().text_range().len().into();
+
+                    let current_offset: usize = input_value.ast_node(db.upcast()).unwrap().text_range().start().into();
+                    let current_node_len: usize = input_value.ast_node(db.upcast()).unwrap().text_range().len().into();
+
+                    diagnostics.push(ApolloDiagnostic::UniqueDefinition(UniqueDefinition {
+                        ty: "argument".into(),
+                        name: name.into(),
+                        src: db.input(),
+                        original_definition: (prev_offset, prev_node_len).into(),
+                        redefined_definition: (current_offset, current_node_len).into(),
+                        help: Some(format!(
+                            "`{name}` argument must only be defined once."
+                        )),
+                    }));
+                } else {
+                    seen.insert(name, &input_value);
+                }
+            }
+        }
+    }
+
+    diagnostics
 }
 
 #[cfg(test)]
