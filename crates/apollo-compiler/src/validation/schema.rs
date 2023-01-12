522d562901f45d4f6eb3e30a80756fc4f59bb821
Irina Shestak <8107784+lrlna@users.noreply.github.com>
2023-01-12 16:25:05 +0100

    feat(compiler): validate root operation type is of Object Type

--- crates/apollo-compiler/src/validation/schema.rs
+++ crates/apollo-compiler/src/validation/schema.rs
@@ -1,8 +1,8 @@
 use std::{collections::HashMap, sync::Arc};
 
 use crate::{
-    diagnostics::{ApolloDiagnostic, DiagnosticData, Label},
-    hir, ValidationDatabase,
+    diagnostics::{DiagnosticData, Label},
+    hir, ApolloDiagnostic, ValidationDatabase,
 };
 
 pub fn validate_schema_definition(
@@ -50,6 +50,10 @@ pub fn validate_root_operation_definitions(
 
     for op_type in root_op_defs.iter() {
         let name = op_type.named_type().name();
+
+        // All root operations in a schema definition must be unique.
+        //
+        // Return a Unique Operation Definition error in case of a duplicate name.
         if let Some(prev_def) = seen.get(&name) {
             if let (Some(original_definition), Some(redefined_definition)) =
                 (prev_def.loc(), op_type.loc())
@@ -79,6 +83,34 @@ pub fn validate_root_operation_definitions(
             }
         } else {
             seen.insert(name, op_type);
+        }
+
+        // Root Operation Named Type must be of Object Type.
+        //
+        // Return a Object Type error if it's any other type definition.
+        let type_def = db.find_type_definition_by_name(op_type.named_type().name());
+        if let Some(type_def) = type_def {
+            if !type_def.is_object_type_definition() {
+                let offset = op_type.loc().unwrap().offset();
+                let node_len = op_type.loc().unwrap().node_len();
+
+                diagnostics.push(ApolloDiagnostic::ObjectType(ObjectType {
+                    name: op_type.named_type().name(),
+                    ty: type_def.ty(),
+                    src: db.source_code(op_type.loc().unwrap().file_id()),
+                    definition: (offset, node_len).into(),
+                    help: "root operation type must be of an Object Type.".into(),
+                }))
+            }
+        } else if op_type.loc().is_some() {
+            let offset = op_type.loc().unwrap().offset();
+            let node_len = op_type.loc().unwrap().node_len();
+
+            diagnostics.push(ApolloDiagnostic::UndefinedDefinition(UndefinedDefinition {
+                ty: op_type.named_type().name(),
+                src: db.source_code(op_type.loc().unwrap().file_id()),
+                definition: (offset, node_len).into(),
+            }))
         }
     }
 
