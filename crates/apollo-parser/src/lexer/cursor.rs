efa365e40847913f2a7aa870206c529a796f8c64
Allan Calix <contact@acx.dev>
2023-07-05 06:33:00 -0700

    feat(parser): zero alloc lexer (#322)
    This commit removes string allocations when lexing tokens. Instead, a Cursor and `CharIndices` iterator is used to track offset positions. This change also introduces a stateful lexer. 
    ---------
    Co-authored-by: Iryna Shestak <shestak.irina@gmail.com>

--- crates/apollo-parser/src/lexer/cursor.rs
+++ crates/apollo-parser/src/lexer/cursor.rs
@@ -1,49 +1,109 @@
-use std::str::Chars;
+use std::str::CharIndices;
 
 use crate::Error;
+
 /// Peekable iterator over a char sequence.
+#[derive(Debug, Clone)]
 pub(crate) struct Cursor<'a> {
-    chars: Chars<'a>,
+    index: usize,
+    offset: usize,
+    source: &'a str,
+    chars: CharIndices<'a>,
+    pending: Option<char>,
     pub(crate) err: Option<Error>,
 }
 
 impl<'a> Cursor<'a> {
     pub(crate) fn new(input: &'a str) -> Cursor<'a> {
         Cursor {
-            chars: input.chars(),
+            index: 0,
+            offset: 0,
+            pending: None,
+            source: input,
+            chars: input.char_indices(),
             err: None,
         }
     }
 }
 
-pub(crate) const EOF_CHAR: char = '\0';
-
 impl<'a> Cursor<'a> {
-    /// Returns nth character relative to the current cursor position.
-    fn nth_char(&self, n: usize) -> char {
-        self.chars().nth(n).unwrap_or(EOF_CHAR)
+    /// Current place (index) in the cursor.
+    pub(crate) fn index(&self) -> usize {
+        self.index
     }
 
-    /// Peeks the next char in input without consuming.
-    pub(crate) fn first(&self) -> char {
-        self.nth_char(0)
+    /// Return true if the current state is pending.
+    pub(crate) fn is_pending(&self) -> bool {
+        self.pending.is_some()
     }
 
-    /// Peeks the second char in input without consuming.
-    pub(crate) fn second(&self) -> char {
-        self.nth_char(1)
+    /// Moves to the next character.
+    pub(crate) fn prev_str(&mut self) -> &'a str {
+        let slice = &self.source[self.index..self.offset];
+
+        self.index = self.offset;
+        self.pending = self
+            .source
+            .get(self.offset..)
+            .and_then(|subslice| subslice.chars().next());
+
+        slice
     }
 
-    /// Checks if there are chars to consume.
-    pub(crate) fn is_eof(&self) -> bool {
-        self.chars.as_str().is_empty()
+    /// Moves to the next character.
+    pub(crate) fn current_str(&mut self) -> &'a str {
+        self.pending = None;
+
+        if let Some((pos, next)) = self.chars.next() {
+            let current = self.index;
+
+            self.index = pos;
+            self.offset = pos;
+            self.pending = Some(next);
+
+            self.source.get(current..pos)
+        } else {
+            let current = self.index;
+            self.index = self.source.len() - 1;
+
+            self.source.get(current..)
+        }
+        .unwrap()
     }
 
     /// Moves to the next character.
     pub(crate) fn bump(&mut self) -> Option<char> {
-        let c = self.chars.next()?;
+        if self.pending.is_some() {
+            return self.pending.take();
+        }
+
+        if self.offset == self.source.len() {
+            return None;
+        }
+
+        let (pos, c) = self.chars.next()?;
+        self.offset = pos;
 
         Some(c)
+    }
+
+    /// Moves to the next character.
+    pub(crate) fn eatc(&mut self, c: char) -> bool {
+        if self.pending.is_some() {
+            panic!("dont call eatc when a character is pending");
+        }
+
+        if let Some((pos, c_in)) = self.chars.next() {
+            self.offset = pos;
+
+            if c_in == c {
+                return true;
+            }
+
+            self.pending = Some(c_in);
+        }
+
+        false
     }
 
     /// Get current error object in the cursor.
@@ -51,13 +111,17 @@ impl<'a> Cursor<'a> {
         self.err.clone()
     }
 
+    /// Drains the current pending characters.
+    pub(crate) fn drain(&mut self) -> &'a str {
+        self.pending = None;
+        let start = self.index;
+        self.index = self.source.len() - 1;
+
+        self.source.get(start..=self.index).unwrap()
+    }
+
     /// Add error object to the cursor.
     pub(crate) fn add_err(&mut self, err: Error) {
         self.err = Some(err)
-    }
-
-    /// Returns a `Chars` iterator over the remaining characters.
-    fn chars(&self) -> Chars<'_> {
-        self.chars.clone()
     }
 }
