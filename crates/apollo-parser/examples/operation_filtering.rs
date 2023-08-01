27f5b8a15b32879e8ca561bccf5f29e976fdc988
Simon Sapin <simon@apollographql.com>
2023-08-01 16:32:04 +0200

    Handle emited selection sets in filtering example

--- crates/apollo-parser/examples/operation_filtering.rs
+++ crates/apollo-parser/examples/operation_filtering.rs
@@ -12,6 +12,12 @@ fn main() {
                 d
             }
             ... Frag
+            ... {
+                h @remove
+            }
+            i {
+                j @remove
+            }
         }
         fragment Frag on Something {
             e
@@ -25,7 +31,10 @@ fn main() {
     }
     let mut mir = ast.into_mir();
     filter_document(&mut mir);
-    dbg!(mir);
+    assert_eq!(
+        mir.serialize().no_indent().to_string(),
+        "{ a(arg: 4) ... { d } ...Frag } fragment Frag on Something { e f }"
+    );
 }
 
 fn filter_document(document: &mut mir::Document) {
@@ -36,28 +45,40 @@ fn filter_document(document: &mut mir::Document) {
             _ => continue,
         };
         match Arc::make_mut(def) {
-            mir::Definition::OperationDefinition(op) => filter_selection_set(&mut op.selection_set),
+            mir::Definition::OperationDefinition(op) => {
+                assert!(
+                    filter_selection_set(&mut op.selection_set),
+                    "operation was emptied"
+                )
+            }
             mir::Definition::FragmentDefinition(frag) => {
-                filter_selection_set(&mut frag.selection_set)
+                // Left as an exercise to the reader:
+                // remove corresponding fragment spreads when a fragment becomes empty.
+                // May require a topological sort for spreads in other fragment definitions.
+                assert!(filter_selection_set(&mut frag.selection_set));
             }
             _ => {}
         }
     }
 }
 
-fn filter_selection_set(selection_set: &mut Vec<Arc<mir::Selection>>) {
+/// Returns wether the parent should be retained.
+fn filter_selection_set(selection_set: &mut Vec<Arc<mir::Selection>>) -> bool {
+    if selection_set.is_empty() {
+        return true;
+    }
     selection_set.retain_mut(|selection| match Arc::make_mut(selection) {
         mir::Selection::Field(field) => {
-            let retain = field.directive_by_name("remove").is_none();
-            if retain {
-                filter_selection_set(&mut field.selection_set);
+            if field.directive_by_name("remove").is_none() {
+                filter_selection_set(&mut field.selection_set)
+            } else {
+                false
             }
-            retain
         }
         mir::Selection::InlineFragment(inline_fragment) => {
-            filter_selection_set(&mut inline_fragment.selection_set);
-            true
+            filter_selection_set(&mut inline_fragment.selection_set)
         }
         mir::Selection::FragmentSpread(_) => true,
-    })
+    });
+    !selection_set.is_empty()
 }
