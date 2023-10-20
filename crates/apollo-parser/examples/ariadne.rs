36e8d0ff52d02e666f0a469246f0daac9cf9c120
Simon Sapin <simon@apollographql.com>
2023-10-20 12:36:31 +0200

    Replace miette with ariadne
    Closes https://github.com/apollographql/apollo-rs/pull/602
    
    Some docs still mention miette and link to an example,
    but they already use a perma-link to an old commit.

--- crates/apollo-parser/examples/ariadne.rs
+++ crates/apollo-parser/examples/ariadne.rs
@@ -1,26 +1,9 @@
 /// This example describes how to use `apollo-parser` with
-/// [`miette`](https://docs.rs/miette/3.2.0/miette) diagnostic library.
-///
-///
+/// [`ariadne`](https://docs.rs/ariadne/0.3.0/ariadne) diagnostic library.
 use std::{fs, path::Path};
 
 use apollo_parser::{cst, Parser};
-use miette::{Diagnostic, NamedSource, Report, SourceSpan};
-use thiserror::Error;
-
-// If your application is using a bunch of other thiserror errors,
-// `ApolloParserError` can live within that enum and be responsible for just
-// `apollo-parser` errors. It should work really nicely together!
-#[derive(Error, Debug, Diagnostic)]
-#[error("{}", self.ty)]
-#[diagnostic(code("apollo-parser parsing error."))]
-struct ApolloParserError {
-    ty: String,
-    #[source_code]
-    src: NamedSource,
-    #[label("{}", self.ty)]
-    span: SourceSpan,
-}
+use ariadne::{Label, Report, ReportKind, Source};
 
 fn parse_schema() -> cst::Document {
     let file = Path::new("crates/apollo-parser/examples/schema_with_errors.graphql");
@@ -41,12 +24,14 @@ fn parse_schema() -> cst::Document {
     for err in cst.errors() {
         // We need to create a report and print that individually, as the error
         // slice can have many errors.
-        let err = Report::new(ApolloParserError {
-            src: NamedSource::new(file_name, src.clone()),
-            span: (err.index(), err.data().len()).into(), // (offset, length of error token)
-            ty: err.message().into(),
-        });
-        println!("{err:?}");
+        let start = err.index();
+        let end = start + err.data().len();
+        Report::build(ReportKind::Error, file_name, start)
+            .with_message(err.message())
+            .with_label(Label::new((file_name, start..end)).with_message(err.message()))
+            .finish()
+            .eprint((file_name, Source::from(&src)))
+            .unwrap();
     }
 
     cst.document()
