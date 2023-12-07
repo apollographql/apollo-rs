737907cbe14cf48e4a5132c5245ca886d87a527d
Renée Kooi <renee.kooi@apollographql.com>
2023-12-07 13:40:21 +0100

    Update to use compiler serialization

--- fuzz/fuzz_targets/strings.rs
+++ fuzz/fuzz_targets/strings.rs
@@ -1,55 +1,34 @@
 #![no_main]
-use apollo_encoder::StringValue;
-use apollo_parser::{ast, Parser};
-use apollo_rs_fuzz::log_gql_doc;
+use apollo_compiler::{name, Schema};
 use libfuzzer_sys::{
     arbitrary::{Arbitrary, Unstructured},
     fuzz_target,
 };
 use log::debug;
-use std::panic;
 
 fuzz_target!(|data: &[u8]| {
     let mut u = Unstructured::new(data);
     let string = String::arbitrary(&mut u).unwrap();
-    let doc_generated = format!(
-        "{}\nscalar DummyDefinition",
-        StringValue::Top {
-            source: string.clone()
-        }
-    );
+    let mut input = Schema::new();
+    let def = input.schema_definition.make_mut();
+    def.description = Some(string.into());
+    // We can refer to a type that doesn't exist as we won't run validation
+    def.query = Some(name!("Dangling").into());
+    let doc_generated = input.to_string();
 
-    let parser = Parser::new(&doc_generated);
-    let tree = parser.parse();
-
-    debug!("======= DOCUMENT =======");
-    debug!("{}", doc_generated);
+    debug!("INPUT STRING: {:?}", input.schema_definition.description);
+    debug!("==== WHOLE DOCUMENT ====");
+    debug!("{doc_generated}");
     debug!("========================");
 
-    let mut should_panic = false;
+    let reparse = Schema::parse(doc_generated, "").unwrap();
+    debug!(
+        "REPARSED STRING: {:?}",
+        reparse.schema_definition.description
+    );
 
-    if tree.errors().len() > 0 {
-        should_panic = true;
-        let errors = tree
-            .errors()
-            .map(|err| err.message())
-            .collect::<Vec<&str>>()
-            .join("\n");
-        debug!("Parser errors ========== \n{:?}", errors);
-        debug!("========================");
-        log_gql_doc(&doc_generated, &errors);
-    }
-
-    let scalar_def = tree.document().definitions().next().unwrap();
-    let ast::Definition::ScalarTypeDefinition(scalar_def) = scalar_def else {
-        panic!("parser produced wrong node type");
-    };
-    let description = scalar_def.description().unwrap();
-    let reparsed_string = String::from(description.string_value().unwrap());
-
-    assert_eq!(reparsed_string, string);
-
-    if should_panic {
-        panic!("error detected");
-    }
+    assert_eq!(
+        reparse.schema_definition.description,
+        input.schema_definition.description
+    );
 });
