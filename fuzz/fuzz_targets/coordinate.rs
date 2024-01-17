547a4abc159199d27b8082a1d93bd76d9dc401bf
Renée <renee@kooi.me>
2024-01-17 14:57:43 +0100

    fix(fuzz): enable env_logger in fuzz targets (#808)
    Use `RUST_LOG=debug cargo fuzz run $target_name` to get the generated
    inputs for fuzzing.

--- fuzz/fuzz_targets/coordinate.rs
+++ fuzz/fuzz_targets/coordinate.rs
@@ -4,6 +4,8 @@ use libfuzzer_sys::fuzz_target;
 use log::debug;
 
 fuzz_target!(|data: &str| {
+    let _ = env_logger::try_init();
+
     let coord = data.parse::<SchemaCoordinate>();
     if let Ok(coord) = &coord {
         assert_eq!(
